92022cf97cc1f13057d50a21d938bb3f92cabde2
UmerAliKhan <aumeralikhan@gmail.com>
2024-11-20 00:40:05 +0500

    dev

--- backend/server.js
+++ backend/server.js
@@ -25,18 +25,14 @@ app.use("/api/message", messageRoutes);
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname1, "/frontend/build")));
-  // app.use(express.static(path.join(__dirname1, "public")));
 
   app.get("*", (req, res) =>
     res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
 } else {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
-  // app.use(express.static(path.join(__dirname1, "public")));
-
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 
 // --------------------------deployment------------------------------
