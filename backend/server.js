bc3dc25efb31fa8482042edd914ec04aecf5910f
UmerAliKhan <aumeralikhan@gmail.com>
2024-11-20 00:47:31 +0500

    dev1

--- backend/server.js
+++ backend/server.js
@@ -23,12 +23,14 @@ app.use("/api/message", messageRoutes);
 
 // --------------------------deployment------------------------------
 const __dirname1 = path.resolve();
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static(path.join(__dirname1, "/frontend/build")));
 
-  app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
-  );
+if (process.env.NODE_ENV === "production") {
+  // Ensure path resolves correctly in production
+  app.use(express.static(path.join(__dirname1, "frontend", "build")));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"));
+  });
 } else {
   app.get("/", (req, res) => {
     res.send("API is running..");
