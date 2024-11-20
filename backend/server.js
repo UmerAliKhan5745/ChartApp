989478c11173caf7aac4ecbb11ab52c44a1bb257
UmerAliKhan <aumeralikhan@gmail.com>
2024-11-20 11:20:40 +0500

    dev

--- backend/server.js
+++ backend/server.js
@@ -6,7 +6,6 @@ const chatRoutes = require("./routes/chatRoutes");
 const messageRoutes = require("./routes/messageRoutes");
 const { notFound, errorHandler } = require("./middleware/errorMiddleware");
 const path = require("path");
-const fs = require("fs");
 
 dotenv.config();
 connectDB();
@@ -25,12 +24,16 @@ app.use("/api/message", messageRoutes);
 // --------------------------deployment------------------------------
 const __dirname1 = path.resolve();
 if (process.env.NODE_ENV === "production") {
-  const buildPath = path.join(__dirname1, "public"); // Point to 'public'
-  app.use(express.static(buildPath));
+  app.use(express.static(path.join(__dirname1, "/frontend/build")));
+  // app.use(express.static(path.join(__dirname1, "public")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(buildPath, "index.html"))
+    res.sendFile(path.resolve(__dirname1, "frontend", "build", "index.html"))
   );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running..");
+  });
 }
 
 // --------------------------deployment------------------------------
