07f5c335b0e47ceec832cb5a9af4e86728abfce4
UmerAliKhan <aumeralikhan@gmail.com>
2024-11-21 01:20:15 +0500

    done

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,15 +2,15 @@ const mongoose = require("mongoose");
 const colors = require("colors");
 
 const connectDB = async () => {
-  try {
-    const conn = await mongoose.connect(
-      process.env.MONGODB_URL || "mongodb://localhost:27017/chatAppumer"
-    );
+  // Make sure this is your MongoDB Atlas URI
+  const mongoURI =
+    process.env.MONGO_URI ||
+    "mongodb+srv://aumeralikhan:Y9xJniALOxLgTi7v@cluster0.nshcp.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
-    console.log(`MongoDB Connected`);
-  } catch (error) {
-    console.error(`Error: ${error.message}`.red.bold);
-  }
+  mongoose
+    .connect(mongoURI, { useNewUrlParser: true, useUnifiedTopology: true })
+    .then(() => console.log("Connected to MongoDB!"))
+    .catch((err) => console.log("Error connecting to MongoDB:", err));
 };
 
 module.exports = connectDB;
