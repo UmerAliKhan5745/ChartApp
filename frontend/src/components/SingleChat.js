a3b7e0c5a5aaa17db11a710aab85686bba57dc43
UmerAliKhan <aumeralikhan@gmail.com>
2024-11-21 23:02:22 +0500

    umer

--- frontend/src/components/SingleChat.js
+++ frontend/src/components/SingleChat.js
@@ -105,7 +105,9 @@ const SingleChat = ({ fetchAgain, setFetchAgain }) => {
   };
 
   useEffect(() => {
-    socket = io(ENDPOINT);
+    socket = io(ENDPOINT, {
+      transports: ["websocket", "polling"], // Ensures compatibility
+    });
     socket.emit("setup", user);
     socket.on("connected", () => setSocketConnected(true));
     socket.on("typing", () => setIsTyping(true));
